c6232d1e656ee66e4102cc6c2c63d72431ef0c01
Himanshu Raj <himanshuraj9194@gmail.com>
2022-05-15 20:21:56 +0530

    clean commmit

---
+++ src/components/ContactPage/ContactSocial.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import socialCards from './SocialCards'
+export default function ContactSocial() {
+  return (
+    <div>ContactSocial
+        <socialCards/>
+    </div>
+  )
+}
